b17e1c65a7096cf49b5c84622af6973542e7d1c6
duchuy1612 <huynguyengl.01012005@gmail.com>
2025-11-29 09:39:33 +0700

    app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 
 const express=require('express');
+const cors = require('cors')
 // const {conn,sql} = require('./config/db')
 
 const app=express();
+app.use(cors())
 app.use(express.json());  
 
 app.get('',(req,res)=>{
